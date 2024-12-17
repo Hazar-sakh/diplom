f238c1b0a5814861a60dc41d35f2892e815840ff
Hazar-sakh <niknakels@gmail.com>
2024-12-17 23:26:05 +1100

    09.12.2024

--- by_plotly.py
+++ by_plotly.py
@@ -15,12 +15,10 @@ def simple_ptl(period, norm_nums, stat):
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=period, y=norm_nums, mode='lines'))
     fig.add_trace(go.Scatter(x=period, y=stat, mode='lines'))
-    if not os.path.exists('simple_plots/'):
-        os.mkdir('simple_plots/')
-        if os.path.exists('simple_plots/simple_plt.png'):
-            os.remove('simple_plots/simple_plt.png')
+    if os.path.exists('simple_plt.png'):
+        os.remove('simple_plt.png')
     name = 'simple_plt'
-    fig.write_image(f'simple_plots/{name}.png')
+    fig.write_image(f'{name}.png')
     t_ptl2 = datetime.datetime.now()
     return f'{t_ptl2 - t_ptl1}'
 
@@ -30,11 +28,9 @@ def hard_ptl(period, stat_h):
     fig = go.Figure()
     for i in stat_h:
         fig.add_trace(go.Scatter(x=period, y=i, mode='lines'))
-    if not os.path.exists('complicated_plots/'):
-        os.mkdir('complicated_plots/')
-        if os.path.exists('complicated_plots/complicated_plt.png'):
-            os.remove('complicated_plots/complicated_plt.png')
+    if os.path.exists('complicated_plt.png'):
+        os.remove('complicated_plt.png')
     name = 'complicated_plt'
-    fig.write_image(f'complicated_plots/{name}.png')
+    fig.write_image(f'{name}.png')
     t_ptl2 = datetime.datetime.now()
     return f'{t_ptl2 - t_ptl1}'
