9d6aad4f4704c232a97fa2ba767d807e8384a92f
Hazar-sakh <niknakels@gmail.com>
2024-12-18 02:21:31 +1100

    09.12.2024

--- Backend/by_seaborn.py
+++ Backend/by_seaborn.py
@@ -13,21 +13,21 @@ import datetime
 # Строим простейший график
 def simple_sns(df):
     t_s1 = datetime.datetime.now()
-    if os.path.exists('simple_sns.png'):
+    if os.path.exists('simple_plots/simple_sns.png'):
         plt.clf()
         plt.close()
     splt = sns.relplot(data=df, kind='line')
-    splt.savefig('simple_sns.png')
+    splt.savefig('simple_plots/simple_sns.png')
     t_s2 = datetime.datetime.now()
     return f'{t_s2 - t_s1}'
 
 # Строим усложненный график
 def hard_sns(df_sns_h):
     t_s1 = datetime.datetime.now()
-    if os.path.exists('compl_sns.png'):
+    if os.path.exists('complicated_plots/compl_sns.png'):
         plt.clf()
         plt.close()
     splt = sns.relplot(data=df_sns_h, kind='line')
-    splt.savefig('compl_sns.png')
+    splt.savefig('complicated_plots/compl_sns.png')
     t_s2 = datetime.datetime.now()
     return f'{t_s2 - t_s1}'
